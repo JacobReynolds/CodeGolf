ba9212146250b4f6ccef4c12ac4ab34d96b21c44
jasper <reyno511@umn.edu>
2017-04-15 14:54:43 -0500

    easterEgg

---
+++ easterEgg/README.md
@@ -0,0 +1,1 @@
+https://codegolf.stackexchange.com/questions/116477/make-me-an-easter-egg
---
+++ easterEgg/mainFirst.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void egg(char d) {
+printf("  __\n");
+printf(" /");
+for(int i=0;i<2;i++){
+  printf("%c",d);
+}
+printf("\\\n");
+for (int i=0;i<2;i++){
+  printf("%c%c%c%c%c%c",i?'|':'/',d,d,d,d,i?'|':'\\');
+  puts("");
+}
+printf("\\____/");
+puts("");
+}
+
+int main(int argc, const char * argv[]) {
+    int i;
+    char output[4] = "-%~";
+    for (i=0;output[i]>0; i++) {
+        //printf("#### Character = %c ####\n",output[i]);
+        egg(output[i]);
+    }
+    return 0;
+}
